eed9994dc1838d2570899a709deb3b93aa61a876
Summer <39847808+SummerNam@users.noreply.github.com>
2018-06-02 17:16:03 +0900

    Add files via upload

---
+++ part01_01.py
@@ -0,0 +1,29 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+print("hello, world!")
+hello, world!
+>>> print("hello", "world!")
+hello world!
+>>> print(3)
+3
+>>> print(3.0)
+3.0
+>>> print(2 + 3)
+5
+>>> print(2.0 + 3.0)
+5.0
+>>> print("2" + "3")
+23
+>>> print("2 + 3 =", 2 + 3)
+2 + 3 = 5
+>>> print(2 * 3)
+6
+>>> print(2 ** 3)
+8
+>>> print(7 / 3)
+2.3333333333333335
+>>> print(7 // 3)
+
+2
+>>> 
