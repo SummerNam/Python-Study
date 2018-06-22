69739a587fecfd0958d8ad300a68e8182e885568
Summer <39847808+SummerNam@users.noreply.github.com>
2018-06-22 13:38:52 +0900

    Add files via upload

---
+++ part02_01.py
@@ -0,0 +1,13 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def main():
+	print("This is a program for converting Celsius temperatures to Fahrenheit.")
+	celsius = eval(input("What is the Celsius temperature? "))
+	fahrenheit = 9/5 * celsius + 32
+	print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+
+>>> main()
+This is a program for converting Celsius temperatures to Fahrenheit.
+What is the Celsius temperature? 0
+The temperature is 32.0 degrees Fahrenheit.
+>>> 
