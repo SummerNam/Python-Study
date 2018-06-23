54a9458fb57c34735f5c9f02457122280d10a566
Summer <39847808+SummerNam@users.noreply.github.com>
2018-06-24 00:34:14 +0900

    Add files via upload

---
+++ part02_10.py
@@ -0,0 +1,13 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def main():
+	print("This program converts kilometers to miles")
+	km = eval(input("What is the distance(km)? "))
+	mile = km * 0.62
+	print("The distance is", mile, "distance(mile).")
+
+>>> main()
+This program converts kilometers to miles
+What is the distance(km)? 1
+The distance is 0.62 distance(mile).
+>>> 
