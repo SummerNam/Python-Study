b28d2be672b16fad281f6a36b426cb64b05dee1c
Summer <39847808+SummerNam@users.noreply.github.com>
2018-06-24 00:42:16 +0900

    Add files via upload

---
+++ part02_11.py
@@ -0,0 +1,13 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def main():
+	print("This program converts MB to KB")
+	MB = eval(input("What is the data amount(MB)? "))
+	KB = MB * 1024
+	print("The data amount is", KB, "KB.")
+
+>>> main()
+This program converts MB to KB
+What is the data amount(MB)? 1
+The data amount is 1024 KB.
+>>> 
